1fb3bba300be1fbd4f42d9db10a0381822432e9e
JMDKENT <juan_mdiaz@hotmail.com>
2023-08-03 12:31:26 -0600

    Update streamlit_app.py
    Adding s3 information and pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@
 #https://emojifinder.com/plate 
 
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("☕ My Parents New Healthy Diner 🥣")
 
@@ -11,3 +14,5 @@ streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
