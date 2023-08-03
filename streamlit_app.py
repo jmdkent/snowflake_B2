6b4a6d29d7460de3d85c63cc5b20fbd2c171c74b
JMDKENT <juan_mdiaz@hotmail.com>
2023-08-03 12:28:36 -0600

    Update streamlit_app.py
    Added emojis to the text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 #Created the main python file
+#https://emojifinder.com/plate 
 
 import streamlit
 
-streamlit.title("My Parents New Healthy Diner")
+streamlit.title("☕ My Parents New Healthy Diner 🥣")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header("🍽Breakfast Menu 🍽")
+streamlit.text("🍜 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
