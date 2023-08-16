a69d92e266ccbe706cc9089d5c6ea66010062dea
JMDKENT <juan_mdiaz@hotmail.com>
2023-08-16 12:03:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,6 +59,19 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 
+streamlit.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur():
+    my_cur.execute("Select * from fruit_load_list")
+    return my_cur.fetchall()
+
+#Add button to load the fruit
+if streamlit.button('Get Fruit Load List"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
 #Dont run anything past here while we troubleshoot
 streamlit.stop()
 
@@ -66,7 +79,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
+
 streamlit.dataframe(my_data_rows)
 
 #Another Section to display fruityvice api response
