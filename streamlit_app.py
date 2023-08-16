ecc92c0ef55c9e3b241b9a3ffe763eae657578a8
JMDKENT <juan_mdiaz@hotmail.com>
2023-08-15 20:54:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+streamlit.text('ESto funciona?')
